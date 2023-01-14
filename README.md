e7ed9daae2b5eea56071e4167dda222ad3afb7d7
Ahmed Shariff <shariff.mfa@outlook.com>
2023-01-13 23:10:48 -0800

    Refactor CLI

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 For recording
 ```sh
-irec -r
+irec r
+```
+
+By default the above records abosolute mouse position. If you want to have relative mouse position pass `-mr` as follows:
+```sh
+irec r -mr
 ```
 
 For replaying
 ```sh
-irec -u
+irec u
 ```
--- pyinputrecorder/cli.py
+++ pyinputrecorder/cli.py
@@ -2,14 +2,20 @@ import click
 from pyinputrecorder._main import setup_listeners, repeat_macro
 
 
-@click.command()
-@click.option("-r", "--record", is_flag=True, default=False)
-@click.option("-u", "--run", is_flag=True, default=False)
-def cli(record, run):
-    assert record is not True or run is not True
-    if record:
-        setup_listeners()
-    elif run:
-        repeat_macro()
-    else:
-        print("Need to use --record/-r or --run/-u")
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("-mr", "--mouse-relative", is_flag=True, default=False)
+def r(mouse_relative):
+    """Record kb/m. Press escape to stop."""
+    mode = "r" if mouse_relative else "a"
+    setup_listeners(mode)
+
+
+@cli.command()
+def u():
+    """Replay recorded kb/m stream."""
+    repeat_macro()
